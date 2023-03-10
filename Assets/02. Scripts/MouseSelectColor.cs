aa82c17d0b161e517bd77ab8987d0f768ea8e219
DolongEE <cowdj08@gmail.com>
2023-03-10 20:20:37 +0900

    main, lobby 캔버스 생성

---
+++ Assets/02. Scripts/MouseSelectColor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseSelectColor : MonoBehaviour
+{
+    RaycastHit rayHit;
+    Ray mouseRay;
+
+    Button button;
+
+    private void Awake()
+    {
+        mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        button = GetComponent<Button>();
+    }
+
+    private void Update()
+    {
+        Physics.Raycast(mouseRay, out rayHit);
+        if (rayHit.collider != null && rayHit.collider == button)
+        {
+            OnMouseEnter();
+        }
+        else if (rayHit.collider != button)
+        {
+            OnMouseExit();
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("MouseEnter");
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("MouseExit");
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("MouseDown");
+    }
+}
---
+++ Assets/02. Scripts/ScriptUI/csMainUIManager.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class csMainUIManager : MonoBehaviour
+{
+    [Header("Canvas Usual")]
+    bool isSetting;
+    bool isCommunity;
+
+    public GameObject pnlSetting;
+    public RectTransform pnlCommunity;
+
+    [Header("Canvas Main")]
+    public Canvas cvMain;
+
+    [Header("Canvas Lobby")]
+    public Canvas cvLobby;
+
+
+    private void Start()
+    {
+        cvMain.enabled = true;
+        cvLobby.enabled = false;
+    }
+
+    private void Update()
+    {
+ 
+        if(!cvLobby.enabled && !isSetting && Input.GetKeyDown(KeyCode.Escape))
+        {
+            isSetting = true;
+            pnlSetting.SetActive(true);
+        }
+        else if (isSetting && Input.GetKeyDown(KeyCode.Escape))
+        {
+            isSetting = false;
+            pnlSetting.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ChangeLobbytoMain();
+        }
+    }
+
+    #region cvUsual
+    public void OnClickUsualSetting()
+    {
+        isSetting = true;
+        pnlSetting.SetActive(true);
+    }
+
+    public void OnClickUsualClose()
+    {
+        isSetting = false;
+        pnlSetting.SetActive(false);
+    }
+
+    public void OnClickUsualExit()
+    {
+        Application.Quit();
+    }
+
+    public void OnClickUsualCommunity()
+    {
+        if (!isCommunity)
+        {
+            isCommunity = true;
+            Vector2 pos = new Vector2(-150f, -40f);
+            pnlCommunity.anchoredPosition = pos;
+        }
+        else
+        {
+            isCommunity = false;
+            Vector2 pos = new Vector2(60f, -40f);
+            pnlCommunity.anchoredPosition = pos;
+        }
+    }
+
+    #endregion
+
+    #region cvMain
+
+    private void ChangeMaintoLobby()
+    {
+        cvMain.enabled = false;
+        cvLobby.enabled = true;
+    }
+    public void OnclickMainPlay()
+    {
+        ChangeMaintoLobby();
+    }
+    public void OnclickMainCareer()
+    {
+        ChangeMaintoLobby();
+    }
+    public void OnclickMainBattlepass()
+    {
+        ChangeMaintoLobby();
+    }
+    public void OnclickMainCollection()
+    {
+        ChangeMaintoLobby();
+    }
+    public void OnclickMainAgent()
+    {
+        ChangeMaintoLobby();
+    }
+    public void OnclickMainStore()
+    {
+        ChangeMaintoLobby();
+    }
+    #endregion
+
+
+    #region cvLobby
+    private void ChangeLobbytoMain()
+    {
+        cvLobby.enabled = false;
+        cvMain.enabled = true;
+    }
+
+
+    #endregion
+
+}
